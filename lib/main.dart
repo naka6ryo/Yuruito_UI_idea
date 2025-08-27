9a9d1330d6a8cf9b2e6bd66b64725706590b1403
林遼亮 <linliaoliang23@gmail.com>
2025-08-27 15:39:26 +0900

    add: firebase_implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-void main() {
-WidgetsFlutterBinding.ensureInitialized();
-runApp(const YuruApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const YuruApp());
 }
