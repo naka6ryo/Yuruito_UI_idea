68d8a83675c5bc2e4ab5996c046645bf5393dc70
林遼亮 <linliaoliang23@gmail.com>
2025-08-27 22:13:59 +0900

    add: geopoint

--- lib/features/map/GetLocation/location.dart
+++ lib/features/map/GetLocation/location.dart
@@ -112,8 +112,7 @@ class LocationService {
       if (uid != null) {
         try {
           await _firestore.collection('locations').doc(uid).set({
-            'lat': averageLat,
-            'lng': averageLng,
+            'location': GeoPoint(averageLat, averageLng),
             'updatedAt': DateTime.now().toIso8601String(),
           }, SetOptions(merge: true));
         } catch (e) {
